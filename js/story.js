7da1ee15b799c8b0f9aa80f25092b2a298ab7b8b
Beth <beth.herlin@gmail.com>
2018-09-28 21:30:47 -0400

    Added existing version

---
+++ js/story.js
@@ -0,0 +1,130 @@
+            $(document).ready(function(){
+                      $(".skillmap") // select your element (supports CSS selectors)
+                .hover(function(){ // trigger the mouseover event
+                    $("#placeholder") // select the element to show (can be anywhere)
+                        .hide(); // show the element
+                }, function(){ // trigger the mouseout event
+                    $("#placeholder") // select the same element
+                        .show(); // hide it
+                });
+            });
+            $(document).ready(function(){
+                      $("#uxdesign") // select your element (supports CSS selectors)
+                .hover(function(){ // trigger the mouseover event
+                    $("#uxdesigndes") // select the element to show (can be anywhere)
+                        .show(); // show the element
+                }, function(){ // trigger the mouseout event
+                    $("#uxdesigndes") // select the same element
+                        .hide(); // hide it
+                });
+            });
+
+            $(document).ready(function(){
+                      $("#commdesign") // select your element (supports CSS selectors)
+                .hover(function(){ // trigger the mouseover event
+                    $("#commdesigndes") // select the element to show (can be anywhere)
+                        .show(); // show the element
+                }, function(){ // trigger the mouseout event
+                    $("#commdesigndes") // select the same element
+                        .hide(); // hide it
+                });
+            });
+
+            $(document).ready(function(){
+                      $("#branddesign") // select your element (supports CSS selectors)
+                .hover(function(){ // trigger the mouseover event
+                    $("#branddesigndes") // select the element to show (can be anywhere)
+                        .show(); // show the element
+                }, function(){ // trigger the mouseout event
+                    $("#branddesigndes") // select the same element
+                        .hide(); // hide it
+                });
+            });
+
+            $(document).ready(function(){
+                      $("#graphicdesign") // select your element (supports CSS selectors)
+                .hover(function(){ // trigger the mouseover event
+                    $("#graphicdesigndes") // select the element to show (can be anywhere)
+                        .show(); // show the element
+                }, function(){ // trigger the mouseout event
+                    $("#graphicdesigndes") // select the same element
+                        .hide(); // hide it
+                });
+            });
+
+            $(document).ready(function(){
+                      $("#designstrategy") // select your element (supports CSS selectors)
+                .hover(function(){ // trigger the mouseover event
+                    $("#designstrategydes") // select the element to show (can be anywhere)
+                        .show(); // show the element
+                }, function(){ // trigger the mouseout event
+                    $("#designstrategydes") // select the same element
+                        .hide(); // hide it
+                });
+            });
+
+            $(document).ready(function(){
+                      $("#leadman") // select your element (supports CSS selectors)
+                .hover(function(){ // trigger the mouseover event
+                    $("#leadmandes") // select the element to show (can be anywhere)
+                        .show(); // show the element
+                }, function(){ // trigger the mouseout event
+                    $("#leadmandes") // select the same element
+                        .hide(); // hide it
+                });
+            });
+
+            $(document).ready(function(){
+                      $("#interactiondesign") // select your element (supports CSS selectors)
+                .hover(function(){ // trigger the mouseover event
+                    $("#interactiondesigndes") // select the element to show (can be anywhere)
+                        .show(); // show the element
+                }, function(){ // trigger the mouseout event
+                    $("#interactiondesigndes") // select the same element
+                        .hide(); // hide it
+                });
+            });
+
+            $(document).ready(function(){
+                      $("#webdesign") // select your element (supports CSS selectors)
+                .hover(function(){ // trigger the mouseover event
+                    $("#webdesigndes") // select the element to show (can be anywhere)
+                        .show(); // show the element
+                }, function(){ // trigger the mouseout event
+                    $("#webdesigndes") // select the same element
+                        .hide(); // hide it
+                });
+            });
+
+            $(document).ready(function(){
+                      $("#userresearch") // select your element (supports CSS selectors)
+                .hover(function(){ // trigger the mouseover event
+                    $("#userresearchdes") // select the element to show (can be anywhere)
+                        .show(); // show the element
+                }, function(){ // trigger the mouseout event
+                    $("#userresearchdes") // select the same element
+                        .hide(); // hide it
+                });
+            });
+
+            $(document).ready(function(){
+                      $("#productdesign") // select your element (supports CSS selectors)
+                .hover(function(){ // trigger the mouseover event
+                    $("#productdesigndes") // select the element to show (can be anywhere)
+                        .show(); // show the element
+                }, function(){ // trigger the mouseout event
+                    $("#productdesigndes") // select the same element
+                        .hide(); // hide it
+                });
+            });
+
+            $(document).ready(function(){
+                      $("#productdev") // select your element (supports CSS selectors)
+                .hover(function(){ // trigger the mouseover event
+                    $("#productdevdes") // select the element to show (can be anywhere)
+                        .show(); // show the element
+                }, function(){ // trigger the mouseout event
+                    $("#productdevdes") // select the same element
+                        .hide(); // hide it
+                });
+            });
